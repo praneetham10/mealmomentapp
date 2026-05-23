180a4743d0b3a35924f7d6ebf9be1c77eb1bef7c
praneetham10 <praneetham10@gmail.com>
2026-05-23 13:16:43 +0530

    Update snowflake_service.py

--- services/snowflake_service.py
+++ services/snowflake_service.py
@@ -1,21 +1,30 @@
 def fetch_products():
 
     products = [
-        {"name": "paneer", "price": 120},
-        {"name": "tomato", "price": 40},
-        {"name": "butter", "price": 60},
-        {"name": "fresh cream", "price": 80},
-        {"name": "onion", "price": 30},
-        {"name": "basmati rice", "price": 150},
-        {"name": "green chili", "price": 20},
-        {"name": "mint leaves", "price": 15},
-        {"name": "biryani masala", "price": 90},
-        {"name": "chicken", "price": 220},
-        {"name": "ginger garlic paste", "price": 55},
-        {"name": "curd", "price": 35},
-        {"name": "coriander leaves", "price": 20},
-        {"name": "capsicum", "price": 45},
-        {"name": "garam masala", "price": 70}
+        {"name": "amul paneer 200g", "price": 90},
+        {"name": "tomato 1kg", "price": 30},
+        {"name": "butter 100g", "price": 55},
+        {"name": "onion 1kg", "price": 40},
+        {"name": "milk 500ml", "price": 25},
+        {"name": "ginger garlic paste 200g", "price": 60},
+        {"name": "fresh cream 200ml", "price": 70},
+        {"name": "cashew nuts 100g", "price": 120},
+        {"name": "red chili powder 100g", "price": 45},
+        {"name": "turmeric powder 100g", "price": 40},
+        {"name": "garam masala 100g", "price": 55},
+        {"name": "coriander leaves 1 bunch", "price": 20},
+        {"name": "basmati rice 1kg", "price": 80},
+        {"name": "chicken 1kg", "price": 220},
+        {"name": "green chilies 100g", "price": 20},
+        {"name": "mint leaves 1 bunch", "price": 15},
+        {"name": "biryani masala 100g", "price": 60},
+        {"name": "curd 500g", "price": 35},
+        {"name": "capsicum 500g", "price": 45},
+        {"name": "cumin seeds 100g", "price": 50},
+        {"name": "mustard seeds 100g", "price": 30},
+        {"name": "oil 1l", "price": 140},
+        {"name": "salt 1kg", "price": 25},
+        {"name": "sugar 1kg", "price": 45}
     ]
 
     return products

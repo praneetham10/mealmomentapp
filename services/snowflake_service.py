7a6ffa2acce8dc5cf9335c7412d4986489cafab8
praneetham10 <praneetham10@gmail.com>
2026-05-23 12:45:24 +0530

    Update snowflake_service.py

--- services/snowflake_service.py
+++ services/snowflake_service.py
@@ -1,37 +1,38 @@
-import streamlit as st
 import snowflake.connector
 
 
 def get_connection():
+
     conn = snowflake.connector.connect(
-        user=st.secrets["SNOWFLAKE_USER"],
-        account=st.secrets["SNOWFLAKE_ACCOUNT"],
-        warehouse=st.secrets["SNOWFLAKE_WAREHOUSE"],
-        database=st.secrets["SNOWFLAKE_DATABASE"],
-        schema=st.secrets["SNOWFLAKE_SCHEMA"],
-        role=st.secrets["SNOWFLAKE_ROLE"],
+        user="YOUR_SNOWFLAKE_USER",
+        account="YOUR_ACCOUNT",
+        warehouse="COMPUTE_WH",
+        database="MEAL_APPTEST",
+        schema="PUBLIC",
+        role="ACCOUNTADMIN",
         authenticator="externalbrowser"
     )
 
-        return conn
-
-    except Exception as e:
-        st.error(f"Snowflake connection failed: {str(e)}")
-        raise
+    return conn
 
 
 def fetch_products():
+
     conn = get_connection()
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT NAME, PRICE FROM PRODUCTS")
+    cursor.execute("""
+        SELECT NAME, PRICE
+        FROM PRODUCTS
+    """)
 
     rows = cursor.fetchall()
 
     products = []
 
     for row in rows:
+
         products.append({
             "name": row[0].lower(),
             "price": row[1]
